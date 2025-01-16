009f4627c17e3ad8955412587be905db5f342676
Bruno Felipe <b.felipe1234@hotmail.com>
2025-01-16 20:15:19 -0300

    ajusta para varios responsaveis

--- src/main.py
+++ src/main.py
@@ -7,37 +7,41 @@ app = Flask(__name__)
 @app.route('/get_all_chamados', methods=['GET'])
 def get_all_chamados():
     token = request.args.get('token')
-    inicio = request.args.get('inicio') if request.args.get('inicio') else None
-    fim = request.args.get('fim') if request.args.get('fim') else None
-    responsaveis = request.args.get('fim') if request.args.get('fim') else None
-    
+    inicio = request.args.get('inicio')
+    fim = request.args.get('fim')
+    responsaveis = request.args.get('responsaveis')
+
     if responsaveis is not None:
         responsaveis = [int(x) for x in responsaveis.split(',')]
-    
+
     if not token:
         return jsonify({"error": "Token é obrigatório"}), 400
 
-    params = ChamadosParamsRequest(abertoStart = inicio, abertoEnd = fim)
-    request_obj = ChamadosRequest(token, params)
-    url = request_obj.buildUrl()
-
     all_data = []
 
-    while True:
-        response = requests.get(url, headers=request_obj.getHeaders())
+    if not responsaveis:
+        responsaveis = [None]
 
-        if response.status_code != 200:
-            return jsonify({"error": f"Erro na requisição: {response.status_code}"}), 500
-
-        data = response.json()
-        all_data.extend(data['data'])
-
-        if data['start'] + data['limit'] >= data['totalPage'] * data['limit']:
-            break
-
-        params.start = data['start'] + data['limit']
+    for responsavel in responsaveis:
+        params = ChamadosParamsRequest(abertoStart = inicio, abertoEnd = fim, responsavel = responsavel)
         request_obj = ChamadosRequest(token, params)
         url = request_obj.buildUrl()
+
+        while True:
+            response = requests.get(url, headers=request_obj.getHeaders())
+
+            if response.status_code != 200:
+                return jsonify({"error": f"Erro na requisição: {response.status_code}"}), 500
+            
+            data = response.json()
+            all_data.extend(data['data'])
+
+            if data['start'] + data['limit'] >= data['totalPage'] * data['limit']:
+                break
+
+            params.start = data['start'] + data['limit']
+            request_obj = ChamadosRequest(token, params)
+            url = request_obj.buildUrl()
 
     return jsonify({"data": all_data})
 
